0c192da514ef57071a2d4205357cfd69fbb776b6
gingermae <gingermae@deborah.deb>
2026-07-02 10:21:12 -0400

    outlining

---
+++ main.py
@@ -0,0 +1,6 @@
+import requests
+
+#pull subject codes from list - after workaround for earlier issues configured
+#retrieve subject variant strings as array
+#if the given variants match the LOC variant list, return LOC
+#else, return NMAH
