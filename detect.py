3c87ad841be3d194a60b60b735a866e57c05459e
Sedalia Mae <gingermaemiller@MacBook-Air.local>
2026-07-13 10:41:00 -0400

    refined detection search for left anchored search option

--- detect.py
+++ detect.py
@@ -1,40 +1,51 @@
 from main import *
 
-def suggestionSearch(unlinked):
+def suggestionSearch(unlinked, type):
+    if type != "keyword" and type != "leftanchored":
+        raise Exception("input type must be 'leftanchored' or 'keyword'")
     headers = {'Accept':'application/json'}
     keyw = urllib.parse.quote(str(unlinked))
-    url = f"https://id.loc.gov/authorities%2Fsubjects/suggest2?q={keyw}&memberOf=http%3A%2F%2Fid.loc.gov%2Fauthorities%2Fsubjects%2Fcollection_LCSHAuthorizedHeadings&searchtype=keyword&count=15&offset=0&sort=relevance&mime=json&usage=true&rawlist=true"
+    url = f"https://id.loc.gov/authorities%2Fsubjects/suggest2?q={keyw}&memberOf=http%3A%2F%2Fid.loc.gov%2Fauthorities%2Fsubjects%2Fcollection_LCSHAuthorizedHeadings&searchtype={type}&count=15&offset=0&sort=relevance&mime=json&usage=true&rawlist=true"
     r = requests.get(url = url, headers = headers)
     if r.status_code != 200:
         raise Exception(f"the request yielded a non-200 response: {r.status_code}")
     return r.json()
 
-def parseResults(json):
+def parseResults(dat, type):
+    # messy; needs to just inherit the type from the original input but whatever
     results = []
-    hits = json["hits"]
+    hits = dat["hits"]
     if len(hits) >= 1:
-        for hit in hits:
-            authHeading = hit["suggestLabel"]
-            results.append(authHeading)
-        return "; ".join(results)
-    return "no matches found"
+        match type:
+            case "keyword":
+                for hit in hits:
+                    authHeading = hit["suggestLabel"]
+                    results.append(authHeading)
+            case "leftanchored":
+                for hit in hits:
+                    results.append(hit)
+    else:
+        return "no matches found"
+    return "; ".join(results)
 
-def main(inFile, outFile):
+#modify to 
+
+def prim(inFile, outFile, type):
     csv = pd.read_csv(inFile, encoding='utf-8')
     matches = []
+    suggested = []
     for i, row in csv.iterrows():
         # this is a typo from the csv that i didn't feel like dealing with, ignore
-        o = getResourceURI(row['Orginal XG Term'])
-        if o == "not found":
-            r = suggestionSearch(row['Orginal XG Term'])
-        else:
-            r = o
-        match = parseResults(r)
+        r = suggestionSearch(row['Orginal XG Term'], type)
+        match = parseResults(r, type)
         matches.append(match)
+        # automatically updates with the first suggested term, saves some work on my end
+        suggested.append(match[0])
         
     csv["LC Suggested Hits"] = matches
+    csv["Updated Term"] = suggested
     csv.to_csv(outFile)
 
 if __name__ == "__main__":
     # change to variables, command line args
-    main("../unlinked-subjects/unlinked-term-counts.csv", "../unlinked-subjects/unlinked-term-hits.csv")
+    prim("../unlinked-subjects/unlinked-subjects-filtered-2.csv", "../unlinked-subjects/unlinked-term-hits2.csv", "leftanchored")
